39d9a2474d750c3341b0c8a404d23e8f0e8b1dfc
Emma Goto <33337378+emgoto@users.noreply.github.com>
2020-11-06 08:58:58 +1100

    Update sizes of GIFs on mobile

--- src/components/gif/styled.js
+++ src/components/gif/styled.js
@@ -4,15 +4,12 @@ export const Container = styled.div`
     text-align: center;
     margin-bottom: 8px;
 
-    @media screen and (min-width: 500px) {
+    @media screen and (min-width: 520px) {
         height: ${props => props.height};
     }
 
     img {
         border-radius: 8px;
-
-        @media screen and (max-width: 499px) {
-            width: 100%;
-        }
+        max-width: 100%;
     }
 `;
