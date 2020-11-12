d7d97d35692e44162b7b9a5d9af1401ed6de9ad1
Emma Goto <33337378+emgoto@users.noreply.github.com>
2020-11-12 13:05:47 +1100

    Add extra padding to top of prevNext component

--- src/components/prev-and-next/styled.js
+++ src/components/prev-and-next/styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import colors from '../../common';
 
 export const Container = styled.div`
+    margin-top: 16px;
     display: flex;
     justify-content: space-between;
 
